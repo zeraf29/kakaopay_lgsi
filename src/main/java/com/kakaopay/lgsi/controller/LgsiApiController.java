e1d8240e47e5c7142af0678925aa67502593532e
zeraf29 <zeraf29@gmail.com>
2019-04-04 02:47:47 +0900

    insert csv test

---
+++ src/main/java/com/kakaopay/lgsi/controller/LgsiApiController.java
@@ -0,0 +1,19 @@
+package com.kakaopay.lgsi.controller;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.opencsv.CSVReader;
+
+@RestController
+@RequestMapping("/api")
+public class LgsiApiController {
+
+		
+}
--- src/main/java/com/kakaopay/lgsi/controller/LgsiWebController.java
+++ src/main/java/com/kakaopay/lgsi/controller/LgsiWebController.java
@@ -1,7 +1,10 @@
 package com.kakaopay.lgsi.controller;
 
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.opencsv.CSVReader;
 
-@RestController
+
+@Controller
+@RequestMapping("/web")
 public class LgsiWebController {
 
 	
@@ -21,4 +27,21 @@ public class LgsiWebController {
         //System.out.println(data);
 		return result;
     }
+	
+	//local csv data file location
+	private static String csvFileName = "/src/main/resources/static/2019경력공채_개발_사전과제1_지자체협약지원API개발_v.1.csv";
+	
+	@RequestMapping(value = "/insert/csv", method = RequestMethod.GET)
+	public void insertCsv() {
+		try {
+			InputStreamReader is = new InputStreamReader(new FileInputStream(csvFileName), "EUC-KR");
+	         CSVReader reader = new CSVReader(is, ',', '"', 0);
+	         List<String[]> list = reader.readAll();
+	         
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
