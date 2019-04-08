39d8e97826fdb7b5cde8ce15c4e47f36e3b90103
jinhyupkim <jinhyupkim@218.38.137.27>
2019-04-08 09:24:36 +0900

    add

---
+++ src/main/java/com/kakaopay/lgsi/common/sortAvgRate.java
@@ -0,0 +1,33 @@
+package com.kakaopay.lgsi.common;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.Map;
+
+import com.kakaopay.lgsi.entity.LocalGovermentSupport;
+
+public class sortAvgRate implements Comparator<LocalGovermentSupport>{	
+	
+	@Override
+	public int compare(LocalGovermentSupport o1, LocalGovermentSupport o2) {
+		// TODO Auto-generated method stub
+		String aRate = o1.getRate().toString().replace(" ", "").replace("대출이자전액", "0").replace("%", "");
+		String bRate = o2.getRate().toString().replace(" ", "").replace("대출이자전액", "0").replace("%", "");
+		String[] aRates = aRate.split("~");
+		String[] bRates = bRate.split("~");
+		int aInt1 = (int)Double.parseDouble(aRates[0])*100;
+		int aInt2 = 0;
+		if(aRates.length>1) {
+			aInt2 = (int)Double.parseDouble(aRates[1])*100;
+		}
+		int bInt1 = (int)Double.parseDouble(bRates[0])*100;
+		int bInt2 =0;
+		if(bRates.length>1) {
+			bInt2 = (int)Double.parseDouble(bRates[1])*100;
+		}
+		
+		return ((aInt1+aInt2)/2) - ((bInt1+bInt2)/2);
+		
+	}
+
+}
---
+++ src/main/java/com/kakaopay/lgsi/common/sortLimit.java
@@ -0,0 +1,37 @@
+package com.kakaopay.lgsi.common;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.Map;
+
+import com.kakaopay.lgsi.entity.LocalGovermentSupport;
+
+public class sortLimit implements Comparator<LocalGovermentSupport>{	
+	
+	@Override
+	public int compare(LocalGovermentSupport o1, LocalGovermentSupport o2) {
+		// TODO Auto-generated method stub
+		int aLimit = Integer.parseInt(o1.getLimit().toString().replace(" ","").replace("억원이내", "00").replace("백만원이내", "").replace("추천금액이내","0"));
+		int bLimit = Integer.parseInt(o1.getLimit().toString().replace(" ","").replace("억원이내", "00").replace("백만원이내", "").replace("추천금액이내","0"));
+		if(aLimit==bLimit) {
+			String aRate = o1.getRate().toString().replace(" ", "").replace("대출이자전액", "0").replace("%", "");
+			String bRate = o2.getRate().toString().replace(" ", "").replace("대출이자전액", "0").replace("%", "");
+			String[] aRates = aRate.split("~");
+			String[] bRates = bRate.split("~");
+			int aInt1 = (int)Double.parseDouble(aRates[0])*100;
+			int aInt2 = 0;
+			if(aRates.length>1) {
+				aInt2 = (int)Double.parseDouble(aRates[1])*100;
+			}
+			int bInt1 = (int)Double.parseDouble(bRates[0])*100;
+			int bInt2 =0;
+			if(bRates.length>1) {
+				bInt2 = (int)Double.parseDouble(bRates[1])*100;
+			}
+			
+			return ((aInt1+aInt2)/2) - ((bInt1+bInt2)/2);
+		}else
+			return aLimit-bLimit;
+	}
+
+}
